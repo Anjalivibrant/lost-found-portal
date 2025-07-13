a5e5c24ad40a861f1babd91abd702c70968f8678
Anjali Singh <121482522+Anjalivibrant@users.noreply.github.com>
2025-07-14 02:44:33 +0530

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -5,13 +5,7 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-s
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNqxGOGhF1xqb1tUY3WAD9oUI_5I9BKFU",
-  authDomain: "lost-n-found-buddy.firebaseapp.com",
-  projectId: "lost-n-found-buddy",
-  storageBucket: "lost-n-found-buddy.firebasestorage.app",
-  messagingSenderId: "519458530416",
-  appId: "1:519458530416:web:cc231041a7e9dd747840b5",
-  measurementId: "G-9P847LFZ0Y"
+ //replace these from your firebase account
 };
 
 const app = initializeApp(firebaseConfig);
